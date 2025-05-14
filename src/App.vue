120ba9f2fad129dfcf7152fcfdb88209e1cd3ada
Believer-j <rjhyj0907@gmail.com>
2025-05-14 21:33:36 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <div>
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <div>
+      <img src="../assets/img/logo.png" height="40px" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss">
+.header {
+  height: 59px;
+  background-color: red;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+@media (min-width: 768px) {
+  .header {
+    background-color: yellow;
+  }
+}
+</style>
